bf760381f8d15070a3c9a3ad83ffd3f1ec8bf86c
meduketto <meduketto@6ddeccfd-2234-0410-8ab0-45b763562eda>
2009-07-04 19:46:23 +0000

    use iks_malloc/free
    git-svn-id: https://iksemel.googlecode.com/svn/trunk@11 6ddeccfd-2234-0410-8ab0-45b763562eda

--- src/md5.c
+++ src/md5.c
@@ -74,7 +74,7 @@ void iks_md5_reset(iksmd5 *md5)
 
 iksmd5 *iks_md5_new(void)
 {
-	iksmd5 *md5 = malloc(sizeof(iksmd5));
+	iksmd5 *md5 = iks_malloc(sizeof(iksmd5));
 
 	if (!md5)
 		return NULL;
@@ -124,7 +124,7 @@ void iks_md5_hash(iksmd5 *md5, const unsigned char *data, size_t slen, int finis
 
 void iks_md5_delete(iksmd5 *md5)
 {
-	free(md5);
+	iks_free(md5);
 }
 
 void iks_md5_digest(iksmd5 *md5, unsigned char *digest)
@@ -137,7 +137,7 @@ void iks_md5_digest(iksmd5 *md5, unsigned char *digest)
 
 void iks_md5_print(iksmd5 *md5, char *buf)
 {
-  int i;
+	int i;
 	unsigned char digest[16];
 
 	iks_md5_digest(md5, digest);
@@ -186,4 +186,3 @@ static void iks_md5_compute(iksmd5 *md5)
 	for (i = 0; i < 4; ++i)
 		md5->state[i] += R[i];
 }
-
