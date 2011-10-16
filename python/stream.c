0b70ce082dba22e6029add2143a3fbbce2654392
meduketto@gmail.com <meduketto@gmail.com@6ddeccfd-2234-0410-8ab0-45b763562eda>
2011-10-16 20:13:21 +0000

    More stream binding work
    git-svn-id: https://iksemel.googlecode.com/svn/trunk@45 6ddeccfd-2234-0410-8ab0-45b763562eda

--- python/stream.c
+++ python/stream.c
@@ -9,15 +9,22 @@
 
 typedef struct {
 	PyObject_HEAD
+	PyObject *jid;
 	iksparser *parser;
+	int features;
+	int authorized;
+	int use_sasl;
+	int use_tls;
 } Stream;
 
 static int Stream_init(Stream *self, PyObject *args, PyObject *kwargs);
 static PyObject *Stream_connect(Stream *self, PyObject *args, PyObject *kwargs);
+static PyObject *Stream_recv(Stream *self);
 static void Stream_dealloc(Stream *self);
 
 static PyMethodDef Stream_methods[] = {
 	{ "connect", (PyCFunction) Stream_connect, METH_KEYWORDS, "Connect stream to an XMPP server" },
+	{ "recv", (PyCFunction) Stream_recv, METH_NOARGS, "Read data from server" },
 	{ NULL }
 };
 
@@ -69,9 +76,49 @@ on_stream(Stream *self, int type, iks *node)
 	PyObject *hook;
 	PyObject *doc;
 	PyObject *ret;
+	iks *x;
 
-	// FIXME: handle different types
-
+	switch (type) {
+#if 0
+		case IKS_NODE_START:
+			if (!iks_is_secure(self->parser)) {
+				iks_start_tls(self->parser);
+				break;
+			}
+			if (!self->use_sasl) {
+				x = iks_make_auth (, , NULL);
+				iks_insert_attrib (x, "id", "auth");
+				iks_send (self->parser, x);
+				iks_delete (x);
+			}
+			break;
+		case IKS_NODE_NORMAL:
+			if (strcmp ("stream:features", iks_name (node)) == 0) {
+				self->features = iks_stream_features (node);
+				if (self->use_sasl) {
+					if (self->use_tls && !iks_is_secure (self->parser)) break;
+					if (self->authorized) {
+						iks *t;
+						if (self->features & IKS_STREAM_BIND) {
+							t = iks_make_resource_bind (sess->acc);
+							iks_send (self->parser, t);
+							iks_delete (t);
+						}
+						if (self->features & IKS_STREAM_SESSION) {
+							t = iks_make_session ();
+							iks_insert_attrib (t, "id", "auth");
+							iks_send (self->parser, t);
+							iks_delete (t);
+						}
+					} else {
+						if (self->features & IKS_STREAM_SASL_MD5)
+							iks_start_sasl (self->prs, IKS_SASL_DIGEST_MD5, sess->acc->user, sess->pass);
+						else if (self->features & IKS_STREAM_SASL_PLAIN)
+							iks_start_sasl (self->prs, IKS_SASL_PLAIN, sess->acc->user, sess->pass);
+					}
+				}
+#endif
+		case IKS_NODE_NORMAL:
 	hook = PyObject_GetAttrString((PyObject *) self, "on_stanza");
 	if (hook) {
 		doc = Document_from_iks(NULL, node);
@@ -87,25 +134,90 @@ on_stream(Stream *self, int type, iks *node)
 			return IKS_HOOK;
 		}
 	}
+	}
+	if (node) iks_delete (node);
 	return IKS_OK;
 }
 
 static int
 Stream_init(Stream *self, PyObject *args, PyObject *kwargs)
 {
+	self->jid = NULL;
 	self->parser = iks_stream_new (
 		IKS_NS_CLIENT,
 		self,
 		(iksStreamHook *) on_stream
 	);
+	self->features = 0;
+	self->authorized = 0;
+	self->use_sasl = 1;
+	self->use_tls = 1;
 	return 0;
 }
 
 static PyObject *
 Stream_connect(Stream *self, PyObject *args, PyObject *kwargs)
 {
-	// FIXME: simple test for now, should initiate connection instead
-	on_stream(self,0,iks_new("lala"));
+	PyObject *o;
+	char *host;
+	int e;
+
+	o = PyMapping_GetItemString(kwargs, "tls");
+	if (o) {
+		if (PyObject_IsTrue(o)) {
+			self->use_tls = 1;
+		} else {
+			self->use_tls = 0;
+		}
+	}
+
+	o = PyMapping_GetItemString(kwargs, "sasl");
+	if (o) {
+		if (PyObject_IsTrue(o)) {
+			self->use_sasl = 1;
+		} else {
+			self->use_sasl = 0;
+		}
+	}
+
+	o = PyMapping_GetItemString(kwargs, "jid");
+	if (!o) {
+		// FIXME: except
+		return NULL;
+	}
+	Py_INCREF(o);
+	self->jid = o;
+
+	o = PyObject_GetAttrString(self->jid, "domain");
+	if (!o) return NULL;
+	host = PyString_AsString(o);
+	if (!host) {
+		Py_DECREF(o);
+		return NULL;
+	}
+
+	e = iks_connect_tcp(self->parser, host, IKS_JABBER_PORT);
+	if (e) {
+		// FIXME: throw exception
+		return NULL;
+	}
+
+	Py_DECREF(o);
+
+	Py_INCREF(Py_None);
+	return Py_None;
+}
+
+static PyObject *
+Stream_recv(Stream *self)
+{
+	int e;
+
+	e = iks_recv(self->parser, 0);
+	if (e) {
+		// FIXME: throw exception
+		return NULL;
+	}
 
 	Py_INCREF(Py_None);
 	return Py_None;
@@ -114,6 +226,7 @@ Stream_connect(Stream *self, PyObject *args, PyObject *kwargs)
 static void
 Stream_dealloc(Stream *self)
 {
+	if (self->jid) { Py_DECREF(self->jid); }
 	iks_parser_delete(self->parser);
 	self->ob_type->tp_free((PyObject *) self);
 }
