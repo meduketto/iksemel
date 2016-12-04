5146a1e62bf7a455fafe3c942347cf3b6a95aa34
Suleyman Poyraz <zaryob.dev@gmail.com>
2016-12-04 19:55:21 +0200

    Create attributes.py

---
+++ tests/attibutes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import piksemel as iks
+
+doc_xml = '<lala b="" c="42"/>'
+
+doc = iks.newDocument("lala")
+doc.setAttribute("a", "lala")
+doc.setAttribute("b", "nanuk")
+doc.setAttribute("b", "")
+doc.setAttribute("a", None)
+doc.setAttribute("c", "42")
+
+assert(doc.toString() == doc_xml)
